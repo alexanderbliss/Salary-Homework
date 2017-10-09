9b8e3b4b98d3a60a49b0be4a599baea167de92f4
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-09 09:06:32 -0500

    turning in assignment

--- client.js
+++ client.js
@@ -11,16 +11,18 @@ function readyNow(){
 function submitClick(){
   console.log('click');
   appendTable();
+  $('.salaryCost').append('<h1>'+ Math.round(sum) +'</h1>');
 
-console.log(id=sum);
+
+  // console.log(id=sum);
 }
 function appendDom(){
   $header = $('<h1>List of Employees</h1>');
   $('.bodyContainer').append($header);
 
 
-$table = $('<table></table>');
-$table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>ID Number</th><th>Job Title</th><th>Annual Salary<th></thead>');
+  $table = $('<table></table>');
+  $table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>ID Number</th><th>Job Title</th><th>Annual Salary<th></thead>');
 
 
 
@@ -31,24 +33,28 @@ function appendTable(){
   '</th><th>' + $('#lastName').val() +'</th><th>' + $('#IDnumber').val() +
   '</th><th>' + $('#jobTitle').val() + '</th><th id="salaryval">' +
   $('#anualSalary').val() + '</th></tr></thead>');
+  id=salaryval = $('#anualSalary').val();
 
-   id=salaryval = $('#anualSalary').val();
-
-salarys.push(parseInt(id=salaryval));
-console.log(salarys);
+  salarys.push(parseInt(id=salaryval));
+  console.log(salarys);
 
   $tbody = $('<tbody id="tableBody"></tbody>');
   $table.append($tbody);
-
+  calSum();
 
 }
 //this whole section dosent work and im not sure why.
 
-id=sums = sum;
-var salarys=[];
-var sum =0;
-for(var i =0; i> salarys.length; i+=1 ){
+function calSum(){
+
+
+for(var i =0; i< salarys.length; i+=1 ){
   sum += salarys[i] ;
 
+  console.log(sum);
+  break;
 }
-$('.salaryCost').append('<h1>'+ $("#sums") +'</h1>');
+sum /= 12;
+}
+var salarys=[];
+var sum =0;
